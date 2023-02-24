b20c93ae0bb046acf2bb4eab6bef832c0b6e6a92
Yasser Angel <yasserangelgm@gmail.com>
2023-02-23 18:58:42 -0600

    Add styles for home public pages

--- src/pages/public/Public.page.jsx
+++ src/pages/public/Public.page.jsx
@@ -1,10 +1,28 @@
-import "./public.styles.css";
+import './public.styles.css';
 const Public = () => {
   return (
     <>
       <main className="main-container">
         <div className="containter-fluid container-md container-sm hero">
-          <h1>Corte de caja</h1>
+          <h3>Centenaria Escuela Primaria</h3>
+          <h2>Juan N. Álvarez</h2>
+          <div className="main-message-container">
+            <div className="welcome">
+              <h1>Bienvenido a Corte de Caja </h1>
+              <h2>Sistema de consulta de Finanzas</h2>
+              <p>
+                En esta aplicación podrás consultar los ingresos y egresos de la
+                cooperativa escolar.
+              </p>
+              <p>
+                Para comenzar, haga clic en el botón Iniciar Sesión en la parte
+                superior de la pantalla
+              </p>
+            </div>
+            <div className="school-logo">
+              <img src="logo.png" alt="" />
+            </div>
+          </div>
         </div>
       </main>
     </>
